01a7997496d084cb7b114cc7ebb8a59677f20981
Harsha <harshamidadhala.k@syncfusion.com>
2025-06-23 14:33:40 +0530

    Updated API Details

--- ChartGeneratorAISample/ChartGenerator/AIService/AICredentials.cs
+++ ChartGeneratorAISample/ChartGenerator/AIService/AICredentials.cs
@@ -2,8 +2,8 @@
 {
     internal abstract class AICredentials
     {
-        internal const string endpoint = "https://mobilemaui.openai.azure.com/";
-        internal const string deploymentName = "gpt-4o";
-        internal const string key = "6673b6975f334c79bd0db8a1cd70aa49";
+        internal const string endpoint = "https://YOUR_ACCOUNT.openai.azure.com/";
+        internal const string deploymentName = "deployment name";
+        internal const string key = "API key";
     }
 }
